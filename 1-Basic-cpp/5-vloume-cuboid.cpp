408e34569025ea7aeb2623c1db61509c305e9fd4
Ritik354 <ritikpawar354@gmail.com>
2023-10-05 21:44:36 +0530

    volume-of-cuboid-code

---
+++ 1-Basic-cpp/5-vloume-cuboid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter length of cuboid";
+    cin>>a;
+    cout<<"Enter breadth of cuboid";
+    cin>>b;
+    cout<<"Enter height of cuboid";
+    cin>>c;
+
+    cout<<"Volume of cuboid is "<<a*b*c;
+    return 0;
+}
