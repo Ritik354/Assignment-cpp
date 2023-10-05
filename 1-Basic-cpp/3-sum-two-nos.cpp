bc77c03a01e99b7c9c1fdddfa29399cfdb112f83
Ritik354 <ritikpawar354@gmail.com>
2023-10-05 13:22:31 +0530

    change

---
+++ 1-Basic-cpp/3-sum-two-nos.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter first number";
+    cin>>a;
+    cout<<"Enter sec number";
+    cin>>b;
+    cout<<"Sum is "<<a+b;
+    return 0;
+}
